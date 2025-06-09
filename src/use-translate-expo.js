f166081bb87e900639b54577329e43b99ffa834e
kaspernj <kasper@diestoeckels.de>
2025-06-09 17:31:57 +0000

    Use context to change locale

--- src/use-translate-expo.js
+++ src/use-translate-expo.js
@@ -1,6 +1,51 @@
+import EventEmitter from "events"
 import translate from "./translate.js"
-import {useCallback} from "react"
+import {createContext, useCallback, useMemo, useState} from "react"
+import useEventEmitter from "@kaspernj/api-maker/build/use-event-emitter"
 import {useLocales} from "expo-localization"
+
+const eventEmitter = new EventEmitter()
+const TranslateContext = createContext()
+
+const WithTranslate = ({children, ...restProps}) => {
+  const restPropsKeys = Object.keys(restProps)
+
+  if (restPropsKeys.length > 0) {
+    throw new Error(`Unknown props given: ${restPropsKeys.join(", ")}`)
+  }
+
+  const locales = useLocales()
+  const [locale, setLocale] = useState()
+  const actualLocales = useMemo(() => {
+    const actualLocales = []
+
+    if (locale) {
+      actualLocales.push(locale)
+    }
+
+    for (const locale of locales) {
+      actualLocales.push(locale.languageCode)
+    }
+
+    return actualLocales
+  }, [locale, locales])
+
+  const contextData = useMemo(() => ({locales: actualLocales}), [actualLocales])
+
+  const onChangeLocale = useCallback(({locale}) => {
+    console.log("onChangeLocale", args)
+
+    setLocale(locale)
+  }, [])
+
+  useEventEmitter(eventEmitter, "changeLocale", onChangeLocale)
+
+  return (
+    <TranslateContext.Provider value={contextData}>
+      {children}
+    </TranslateContext.Provider>
+  )
+}
 
 const useTranslateExpo = () => {
   const locales = useLocales()
@@ -19,4 +64,5 @@ const useTranslateExpo = () => {
   return currentTranslation
 }
 
+export {WithTranslate}
 export default useTranslateExpo
